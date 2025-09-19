db6295246e3d95747b5b096b6418638264dd9378
truman <trumanjimothy@gmail.com>
2025-09-19 01:55:17 -0400

    added scene loader

---
+++ Godot Project/scripts/SceneLoader.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+
+//Node used to change scenes
+//Call the node's ChangeToScene function to change scenes
+public partial class SceneLoader : Node
+{
+	public void ChangeToScene(string sceneName)
+	{
+		GetTree().ChangeSceneToFile($"res://scenes/{sceneName}");
+	}
+}
